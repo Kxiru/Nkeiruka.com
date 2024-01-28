f7c578b976e70bf635a974cb8aab0bc26b75a9d8
kxiru <nkeirukaw@gmail.com>
2024-01-28 04:24:09 +0000

    Fixed project card

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,6 +1,6 @@
 export default function ServiceCard(props) {
   return (
-    <div className="text-center shadow-lg p-5 rounded-xl my-10 flex-1 bg-white">
+    <div className="relative text-center shadow-lg p-5 rounded-xl my-10 flex-1 bg-white pb-16">
       <div>
         <img
           className="mx-auto w-50 h-40 object-cover rounded-md"
@@ -8,13 +8,13 @@ export default function ServiceCard(props) {
           alt=""
         />
       </div>
-      <div className="h-40">
+      <div className="h-fit">
         <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600">
           {props.cardTitle}
         </h3>
         <p className="py-2">{props.cardDescription}</p>
       </div>
-      <div className="my-5">
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 ">
         <a
           href={props.cardLink}
           className="bg-gradient-to-r bg-teal-500 text-white px-4 py-2 rounded"
